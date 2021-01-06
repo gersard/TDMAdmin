a50101a1105f0083ff7f6e31dff98d7600cc910b
Gerardo Mascayano <gerardo.mascayano@assertsoft.cl>
2021-01-06 00:19:40 -0300

    - Diseño item orden

--- app/src/main/java/cl/gerardomascayano/tdmadmin/ui/orders/OrdersFragment.kt
+++ app/src/main/java/cl/gerardomascayano/tdmadmin/ui/orders/OrdersFragment.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import cl.gerardomascayano.tdmadmin.R
+import cl.gerardomascayano.tdmadmin.databinding.FragmentOrdersBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OrdersFragment : Fragment() {
 
-//    private var _viewBinding: OrdersFragmentBinding
+    private var _viewBinding: FragmentOrdersBinding? = null
+    private val viewBinding: FragmentOrdersBinding
+        get() = _viewBinding!!
     private val ordersViewModel = viewModels<OrdersViewModel>()
 
     override fun onCreateView(
@@ -23,11 +23,9 @@ class OrdersFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-
-        return root
+        _viewBinding = FragmentOrdersBinding.inflate(inflater, container, false)
+        return viewBinding.root
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
