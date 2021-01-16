5d9c2bc2481fa7ca364dc91a3ea65d30e6925b0f
Gerardo Mascayano <gerardo.mascayano@gmail.com>
2021-01-16 18:36:58 -0300

    - Se añade y configura crashlytics

--- app/src/main/java/cl/gerardomascayano/tdmadmin/TDMApplication.kt
+++ app/src/main/java/cl/gerardomascayano/tdmadmin/TDMApplication.kt
@@ -1,9 +1,8 @@
 package cl.gerardomascayano.tdmadmin
 
 import android.app.Application
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.ktx.messaging
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -14,16 +13,26 @@ class TDMApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            debugConfig()
+        } else {
+            releaseConfig()
         }
 
         Firebase.messaging.subscribeToTopic(getString(R.string.fcm_topic_order)).addOnCompleteListener {
             if (it.isSuccessful) Timber.d("Suscripción exitosa") else Timber.e(it.exception)
         }
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-            Timber.d(task.result)
-        }
+//        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+//            Timber.d(task.result)
+//        }
     }
 
+    private fun debugConfig() {
+        Timber.plant(Timber.DebugTree())
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
+    }
+
+    private fun releaseConfig() {
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
+    }
 }
