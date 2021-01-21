8422d50cfffcaccc1b62d429158877832d7b4ae1
Gerardo Mascayano <gerardo.mascayano@gmail.com>
2021-01-20 21:28:44 -0300

    - Diseño de celda de nota - Adapter lista de notas

--- app/src/main/java/cl/gerardomascayano/tdmadmin/domain/order/note/OrderNoteType.kt
+++ app/src/main/java/cl/gerardomascayano/tdmadmin/domain/order/note/OrderNoteType.kt
@@ -1,8 +1,8 @@
 package cl.gerardomascayano.tdmadmin.domain.order.note
 
 
-enum class OrderNoteType {
-    PRIVADA, CLIENTE;
+enum class OrderNoteType(val description: String) {
+    PRIVADA("Nota privada"), CLIENTE("Nota cliente");
 
     companion object {
         fun from(isCliente: Boolean) = if (isCliente) CLIENTE else PRIVADA
---
+++ app/src/main/java/cl/gerardomascayano/tdmadmin/ui/orders/note/OrderNotesAdapter.kt
@@ -0,0 +1,37 @@
+package cl.gerardomascayano.tdmadmin.ui.orders.note
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cl.gerardomascayano.tdmadmin.core.extension.format
+import cl.gerardomascayano.tdmadmin.databinding.ItemOrderNoteBinding
+import cl.gerardomascayano.tdmadmin.domain.order.OrderDateUtil
+import cl.gerardomascayano.tdmadmin.domain.order.note.OrderNote
+
+class OrderNotesAdapter : RecyclerView.Adapter<OrderNotesAdapter.OrderNoteViewHolder>() {
+
+    private val listNotes = mutableListOf<OrderNote>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = OrderNoteViewHolder(
+        ItemOrderNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: OrderNoteViewHolder, position: Int) = holder.bindNote(listNotes[position])
+
+    override fun getItemCount(): Int = listNotes.size
+
+    fun addNewNote(note: OrderNote) {
+        listNotes.add(0, note)
+        notifyItemInserted(0)
+    }
+
+    inner class OrderNoteViewHolder(private val viewBinding: ItemOrderNoteBinding) : RecyclerView.ViewHolder(viewBinding.root) {
+
+        fun bindNote(note: OrderNote) {
+            viewBinding.tvNoteType.text = note.noteType.description
+            viewBinding.tvNote.text = note.note
+            viewBinding.tvNoteDateCreated.text = note.dateCreated.format(OrderDateUtil.PATTERN_ORDER_DATE_CREATED_LIST)
+        }
+    }
+
+}
